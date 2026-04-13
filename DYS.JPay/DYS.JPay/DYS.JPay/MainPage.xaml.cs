13d8d4ba9f071f681266c91fc926abdeaf154f68
Jeffrey Valeroso <devyardph@gmail.com.com>
2026-04-13 14:56:18 +0800

    fixed height

--- DYS.JPay/DYS.JPay/DYS.JPay/MainPage.xaml.cs
+++ DYS.JPay/DYS.JPay/DYS.JPay/MainPage.xaml.cs
@@ -1,10 +1,22 @@
-﻿namespace DYS.JPay
+﻿using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+
+namespace DYS.JPay
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+#if IOS
+    if (DeviceInfo.Idiom == DeviceIdiom.Tablet)
+    {
+        Padding = new Thickness(0, 0, 0, -50); // adjust value as needed
+    }
+#endif
         }
+
     }
 }
+
