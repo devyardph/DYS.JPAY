f938175b6718958a61a54d31f606c6286de61837
Jeffrey Valeroso <devyardph@gmail.com.com>
2026-04-24 22:41:13 +0800

    fixed

---
+++ DYS.JPay/DYS.JPay/DYS.JPay.Shared/Shared/Helpers/CsvHelper.cs
@@ -0,0 +1,23 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace DYS.JPay.Shared.Shared.Helpers
+{
+    public static class CsvHelper
+    {
+        public static byte[] ExportToCsv<T>(IEnumerable<T> records)
+        {
+            using var memoryStream = new MemoryStream();
+            using var writer = new StreamWriter(memoryStream, Encoding.UTF8, leaveOpen: true);
+            using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+            csv.WriteRecords(records);
+            writer.Flush();
+
+            return memoryStream.ToArray();
+        }
+    }
+}
